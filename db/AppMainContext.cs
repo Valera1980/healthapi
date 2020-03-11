8b3a39a7e0f6817932ae84d7f4708ef90c80df76
Valera <gavrilow777@gmail.com>
2020-03-11 20:20:52 +0200

    add users migration

---
+++ db/AppMainContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace HealthApi.Models {
+    public class AppMainContext: DbContext 
+    {
+        public DbSet<User> Users { get; set;}
+        public AppMainContext(DbContextOptions<AppMainContext> options): base(options)
+        {
+
+        }
+    }
+}
