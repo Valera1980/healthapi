0fe269e68f6abcf549a44f0217ecba7cc47722ab
Valera <gavrilow777@gmail.com>
2020-03-11 19:43:04 +0200

    add auth controller

---
+++ Controllers/Auth.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("auth")]
+public class AuthController : ControllerBase
+{
+    [HttpGet]
+    public async Task<string>  Get()
+    {
+        var client = new HttpClient();
+        var disco = await client.GetDiscoveryDocumentAsync("http://localhost:5000");
+
+
+        var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+        {
+            Address = disco.TokenEndpoint,
+
+            ClientId = "client",
+            ClientSecret = "secret",
+            Scope = "api1"
+        });
+
+        if (tokenResponse.IsError)
+        {
+            Console.WriteLine(tokenResponse.Error);
+            // return;
+        }
+
+        Console.WriteLine(tokenResponse.Json);
+
+        // return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+        return tokenResponse.Raw;
+    }
+}
