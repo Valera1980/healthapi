4536f6952565887a2856c00c870c646e3a29cf4d
Valera <gavrilow777@gmail.com>
2020-03-15 11:53:16 +0200

    test commit after change directory

--- models/user-model.cs
+++ models/user-model.cs
@@ -2,7 +2,6 @@ using System.ComponentModel.DataAnnotations;
 
 public class User
 {
-    [Key]
     public int Id { get; set; }
     public string Name { get; set; }
     public string Email { get; set; }
